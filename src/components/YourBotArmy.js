f13555e3065e4e4278f586db4310737a0f4b418f
Sammy048 <101838291+Sammy048@users.noreply.github.com>
2022-10-04 10:36:01 +0300

    Update YourBotArmy.js

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,19 +1,22 @@
 import React from "react";
-import BotCard from "./BotCard";
-function YourBotArmy({HandleAddCard,botArmy,HandleRemoveBot}) {
-  //your bot army code here...
+import BotCard from "./BotCard"
+
+function YourBotArmy({ bots, removeBot , deleteBot }) {
+  
+
+  const  myArmy = bots.map(bot => 
+    <BotCard 
+    key={bot.id}
+    bot={bot}
+    clickEvent={removeBot}
+    deleteBot={deleteBot}
+    />)
 
   return (
     <div className="ui segment inverted olive bot-army">
       <div className="ui five column grid">
         <div className="row bot-army-row">
-          {/*...and here...*/}
-          
-          {botArmy.map((bot)=>  <BotCard bot={bot}  HandleAddCard={HandleAddCard} HandleRemoveBot={HandleRemoveBot} key={bot.id}/> )}
-                
-              
-            
-          
+          {myArmy}
         </div>
       </div>
     </div>
